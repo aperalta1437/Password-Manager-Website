87338627ec0bf7d3f1e82cd667633764cf15261a
Angel Peralta <amiguelp007@gmail.com>
2021-04-12 21:50:15 -0400

    Added login page along with its stylesheet.

--- routes/index.py
+++ routes/index.py
@@ -4,6 +4,7 @@ from flask import Flask, send_file, render_template
 app=Flask(__name__, static_folder='../public/static/', template_folder='../public/views/')
 app.config["DEBUG"]= True
 @app.route('/', methods=['GET'])
+@app.route('/index.html', methods=['GET'])
 def index():
         return render_template('index.html')
 
